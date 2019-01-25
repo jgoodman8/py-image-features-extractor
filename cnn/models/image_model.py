d347734d815fbdb4fa062ee59617592add7f9368
jgoodman8 <jguzmanfd@gmail.com>
2019-01-25 20:21:41 +0100

    Changed optimizer

--- cnn/models/image_model.py
+++ cnn/models/image_model.py
@@ -1,12 +1,10 @@
 import math
 
 from tensorflow.keras.applications.vgg19 import VGG19
-from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.callbacks import ModelCheckpoint
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Flatten
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.layers import Dense, Flatten, Dropout
 from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.preprocessing import image
 
 
@@ -16,7 +14,7 @@ class ImageModel:
     self.train_route = base_route + "/train"
     self.validation_route = base_route + "/validation"
     
-    self.epochs = 10
+    self.epochs = 1000
     self.batch_size = 256
     self.train_size = 100000
     self.validation_size = 10000
@@ -32,7 +30,7 @@ class ImageModel:
     )
     
     self.checkpoint = ModelCheckpoint(
-      "inception.h5",
+      "vgg16.h5",
       monitor='val_acc',
       verbose=1,
       save_best_only=True,
@@ -42,7 +40,6 @@ class ImageModel:
     )
     
     self.model = None
-    self.base_layers_size = 0
   
   def get_directory_iterator(self, route):
     image_generator = image.ImageDataGenerator(rescale=1.0 / 255)
@@ -51,21 +48,23 @@ class ImageModel:
       directory=route,
       target_size=(self.width, self.height),
       batch_size=self.batch_size,
-      class_mode="categorical",
-      shuffle=True
+      class_mode="categorical"
     )
   
   def set_model(self):
     # create the base pre-trained model
     base_model = VGG19(weights='imagenet', include_top=False, input_shape=(self.width, self.height, 3))
     
-    self.base_layers_size = len(base_model.layers)
+    for layer in base_model.layers:
+      layer.trainable = False
     
     # add a global spatial average pooling layer
     x = base_model.output
     
     x = Flatten()(x)
     # let's add a fully-connected layer
+    x = Dense(1024, activation='relu')(x)
+    x = Dropout(0.5)(x)
     x = Dense(1024, activation='relu')(x)
     # and a logistic layer -- let's say we have 200 classes
     predictions = Dense(200, activation='softmax')(x)
@@ -74,29 +73,16 @@ class ImageModel:
     self.model = Model(inputs=base_model.input, outputs=predictions)
   
   def train(self):
-    
     self.set_model()
     self.model.summary()
-    self.model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=["accuracy"])
+    self.model.compile(
+      optimizer=Adam(),
+      loss='categorical_crossentropy',
+      metrics=["accuracy"]
+    )
     
     train_directory_iterator = self.get_directory_iterator(self.train_route)
     validation_directory_iterator = self.get_directory_iterator(self.validation_route)
-    
-    self.model.fit_generator(
-      train_directory_iterator,
-      steps_per_epoch=self.steps_per_epoch,
-      epochs=10,
-      validation_data=validation_directory_iterator,
-      validation_steps=self.validation_steps,
-      callbacks=[self.checkpoint, self.early]
-    )
-    
-    for layer in self.model.layers[:self.base_layers_size]:
-      layer.trainable = False
-    for layer in self.model.layers[self.base_layers_size:]:
-      layer.trainable = True
-    
-    self.model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy', metrics=["accuracy"])
     
     self.model.fit_generator(
       train_directory_iterator,
