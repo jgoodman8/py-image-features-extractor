22b9287489e172e0c9a732fab3ee5e8bd63b8458
jgoodman8 <jguzmanfd@gmail.com>
2019-02-24 18:00:30 +0100

    Set epochs params for fine tuning

--- cnn/models/image_model.py
+++ cnn/models/image_model.py
@@ -1,21 +1,26 @@
-import os
 import math
+import os
 
 from tensorflow.keras.applications.vgg19 import VGG19
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-from tensorflow.keras.layers import Dense, Flatten, Dropout
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import GlobalAveragePooling2D
 from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.preprocessing import image
 
 
 class ImageModel:
-  def __init__(self, base_route, train_folder="train", validation_folder="validation"):
+  def __init__(self, base_route, train_folder="train", validation_folder="validation", epochs=10, fine_tune_epochs=100,
+               fine_tune=0):
     self.width = self.height = 64
     self.train_route = os.path.join(base_route, train_folder)
     self.validation_route = os.path.join(base_route, validation_folder)
     
-    self.epochs = 1000
+    self.fine_tuning = False if fine_tune == 0 else True
+    
+    self.epochs = epochs
+    self.fine_tune_epochs = fine_tune_epochs
     self.__batch_size = 256
     
     self.model_route = "model.h5"
@@ -38,29 +43,37 @@ class ImageModel:
     # add a global spatial average pooling layer
     x = base_model.output
     
-    x = Flatten()(x)
-    # let's add a fully-connected layer
-    x = Dense(1024, activation='relu')(x)
-    x = Dropout(0.5)(x)
-    x = Dense(1024, activation='relu')(x)
+    x = GlobalAveragePooling2D()(x)
+    
+    # add fully-connected layer
+    x = Dense(512, activation='relu')(x)
+    
+    # add output layer
+    predictions = Dense(200, activation='softmax')(x)
+    
+    # x = Flatten()(x)
+    # # let's add a fully-connected layer
+    # x = Dense(1024, activation='relu')(x)
+    # x = Dropout(0.5)(x)
+    # x = Dense(1024, activation='relu')(x)
     # and a logistic layer -- let's say we have 200 classes
-    predictions = Dense(number_of_classes, activation='softmax')(x)
+    # predictions = Dense(number_of_classes, activation='softmax')(x)
     
     # this is the model we will train
     self.model = Model(inputs=base_model.input, outputs=predictions)
     
-    self.model.compile(
-      optimizer=Adam(),
-      loss='categorical_crossentropy',
-      metrics=["accuracy"]
-    )
-    
+    self.model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+  
   def fine_tune(self):
-    for layer in self.model.layers:
+    
+    num_layers = len(self.model.layers)
+    for layer in self.model.layers[:int(num_layers * 0.9)]:
+      layer.trainable = False
+    for layer in self.model.layers[int(num_layers * 0.9):]:
       layer.trainable = True
-      
+    
     self.model.compile(
-      optimizer=Adam(),
+      optimizer=SGD(lr=0.0001, momentum=0.9),
       loss='categorical_crossentropy',
       metrics=["accuracy"]
     )
@@ -74,25 +87,19 @@ class ImageModel:
     
     self.build_model(train_directory_iterator.num_classes)
     
-    self.model.fit_generator(
-      train_directory_iterator,
-      steps_per_epoch=self.train_steps,
-      epochs=self.epochs,
-      validation_data=validation_directory_iterator,
-      validation_steps=self.validation_steps,
-      callbacks=[self.checkpoint, self.early]
-    )
+    self.model.fit_generator(train_directory_iterator)
     
-    self.fine_tune()
-    
-    self.model.fit_generator(
-      train_directory_iterator,
-      steps_per_epoch=self.train_steps,
-      epochs=self.epochs,
-      validation_data=validation_directory_iterator,
-      validation_steps=self.validation_steps,
-      callbacks=[self.checkpoint, self.early]
-    )
+    if self.fine_tuning:
+      self.fine_tune()
+      
+      self.model.fit_generator(
+        train_directory_iterator,
+        steps_per_epoch=self.train_steps,
+        epochs=self.fine_tune_epochs,
+        validation_data=validation_directory_iterator,
+        validation_steps=self.validation_steps,
+        callbacks=[self.checkpoint, self.early]
+      )
     
     self.model.save("vgg19_model.h5")
     
@@ -118,11 +125,11 @@ class ImageModel:
       verbose=1,
       mode='auto'
     )
-
+  
   @staticmethod
   def get_directory_iterator(route):
     image_generator = image.ImageDataGenerator(rescale=1.0 / 255)
-  
+    
     return image_generator.flow_from_directory(
       directory=route,
       target_size=(64, 64),
--- setup.py
+++ setup.py
@@ -9,20 +9,25 @@ from cnn.utils import change_validation_scaffolding
 class Train(Command):
   description = 'Train and Evaluate a CNN-based model'
   user_options = [
-    ('route=', None, 'Training base route')
+    ('route=', None, 'Training base route'),
+    ('epochs=', None, 'Epochs to fine-tune'),
+    ('fineTuneEpochs=', None, 'Epochs to fine-tune'),
+    ('fineTune=', None, 'Perform fine-tune or not')
   ]
   
   def initialize_options(self):
     self.route = ''
+    self.epochs = 10
+    self.fineTuneEpochs = 100
+    self.fineTune = 0
   
   def finalize_options(self):
     pass
   
   def run(self):
-    print(self.route)
-    
-    extractor = ImageModel(self.route)
-    metrics = extractor.train()
+    model = ImageModel(base_route=self.route, epochs=int(self.epochs), fine_tune_epochs=int(self.fineTuneEpochs),
+                       fine_tune=int(self.fineTune))
+    metrics = model.train()
     
     print(metrics)
 
