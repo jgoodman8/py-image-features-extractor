a940df88f3574f9f4593fd8dd1acad6c24701174
jgoodman8 <jguzmanfd@gmail.com>
2019-03-10 00:02:58 +0100

    Refactor in transfer-learning/fine-tuning options

--- image_feature_extractor/models/image_model.py
+++ image_feature_extractor/models/image_model.py
@@ -13,101 +13,89 @@ from tensorflow.keras.preprocessing import image
 class ImageModel:
     def __init__(self, base_route, train_folder="train", validation_folder="validation", epochs=10,
                  fine_tune_epochs=100, fine_tune: bool = False):
-        self.width = self.height = 64
-        self.train_route = os.path.join(base_route, train_folder)
-        self.validation_route = os.path.join(base_route, validation_folder)
+        self.__model = None
+        self.__base_model = None
+        self.__width = self.__height = 64
+        self.__train_route = os.path.join(base_route, train_folder)
+        self.__validation_route = os.path.join(base_route, validation_folder)
         
-        self.fine_tuning = fine_tune
+        self.__fine_tuning = fine_tune
         
-        self.epochs = epochs
-        self.fine_tune_epochs = fine_tune_epochs
-        self.__batch_size = 256
+        self.__epochs = epochs
+        self.__fine_tune_epochs = fine_tune_epochs
+        self.batch_size = 256
         
-        self.model_route = "model.h5"
-        self.early = ImageModel.get_early_stop()
-        self.checkpoint = self.get_model_checkpoint()
+        self.__model_route = "model.h5"
+        self.__early_stop = EarlyStopping(monitor='val_acc', min_delta=0, patience=10, verbose=1, mode='auto')
+        self.__checkpoint = self._get_model_checkpoint()
         
-        self.model = None
-        self.__train_size = 0
-        self.__validation_size = 0
+        self.train_size = 0
+        self.validation_size = 0
         self.train_steps = 0
         self.validation_steps = 0
     
-    def build_model(self):
-        # create the base pre-trained model
-        base_model = VGG19(weights='imagenet', include_top=False, input_shape=(self.width, self.height, 3))
-        
-        for layer in base_model.layers:
-            layer.trainable = False
-        
-        # add a global spatial average pooling layer
-        x = base_model.output
-        
-        x = GlobalAveragePooling2D()(x)
-        
-        # add fully-connected layer
-        x = Dense(512, activation='relu')(x)
-        
-        # add output layer
-        predictions = Dense(200, activation='softmax')(x)
-        
-        # x = Flatten()(x)
-        # # let's add a fully-connected layer
-        # x = Dense(1024, activation='relu')(x)
-        # x = Dropout(0.5)(x)
-        # x = Dense(1024, activation='relu')(x)
-        # and a logistic layer -- let's say we have 200 classes
-        # predictions = Dense(number_of_classes, activation='softmax')(x)
-        
-        # this is the model we will train
-        self.model = Model(inputs=base_model.input, outputs=predictions)
-        
-        self.model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
-    
-    def fine_tune(self):
-        
-        num_layers = len(self.model.layers)
-        for layer in self.model.layers[:int(num_layers * 0.9)]:
-            layer.trainable = False
-        for layer in self.model.layers[int(num_layers * 0.9):]:
-            layer.trainable = True
-        
-        self.model.compile(
-            optimizer=SGD(lr=0.0001, momentum=0.9),
-            loss='categorical_crossentropy',
-            metrics=["accuracy"]
-        )
-    
     def train(self):
-        train_directory_iterator = ImageModel.get_directory_iterator(self.train_route)
-        self.train_size = train_directory_iterator.samples
+        train_directory_iterator = self._get_directory_iterator(self.__train_route)
+        validation_directory_iterator = self._get_directory_iterator(self.__validation_route)
         
-        validation_directory_iterator = ImageModel.get_directory_iterator(self.validation_route)
+        self.train_size = train_directory_iterator.samples
         self.validation_size = validation_directory_iterator.samples
         
-        self.build_model(train_directory_iterator.num_classes)
+        self._build_model(train_directory_iterator.num_classes)
         
-        self.model.fit_generator(train_directory_iterator)
+        if self.__fine_tuning:
+            self._set_fine_tune()
+        else:
+            self._set_transfer_learning()
         
-        if self.fine_tuning:
-            self.fine_tune()
-            
-            self.model.fit_generator(
-                train_directory_iterator,
-                steps_per_epoch=self.train_steps,
-                epochs=self.fine_tune_epochs,
-                validation_data=validation_directory_iterator,
-                validation_steps=self.validation_steps,
-                callbacks=[self.checkpoint, self.early]
-            )
+        self.__model.fit_generator(
+            train_directory_iterator,
+            steps_per_epoch=self.train_steps,
+            epochs=self.__fine_tune_epochs,
+            validation_data=validation_directory_iterator,
+            validation_steps=self.validation_steps,
+            callbacks=[self.__checkpoint, self.__early_stop]
+        )
         
-        self.model.save("vgg19_model.h5")
+        self.__model.save(self.__model_route)
         
-        return self.model.evaluate_generator(train_directory_iterator)
+        metrics = self.__model.evaluate_generator(train_directory_iterator)
+        
+        return metrics
     
-    def get_model_checkpoint(self):
+    def _build_model(self, num_classes: int):
+        self.__base_model = VGG19(weights='imagenet', include_top=False, input_shape=(self.__width, self.__height, 3))
+        
+        x = self.__base_model.output
+        x = GlobalAveragePooling2D()(x)
+        x = Dense(512, activation='relu')(x)
+        predictions = Dense(num_classes, activation='softmax')(x)
+        
+        self.__model = Model(inputs=self.__base_model.input, outputs=predictions)
+    
+    def _set_transfer_learning(self):
+        for layer in self.__base_model.layers:
+            layer.trainable = False
+        
+        self.__model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+    
+    def _set_fine_tune(self):
+        layers_to_freeze = int(len(self.__model.layers) * 0.9)
+        
+        for layer in self.__model.layers[:layers_to_freeze]:
+            layer.trainable = False
+        for layer in self.__model.layers[layers_to_freeze:]:
+            layer.trainable = True
+        
+        self.__model.compile(
+            optimizer=SGD(lr=0.0001, momentum=0.9),
+            loss='categorical_crossentropy',
+            metrics=['accuracy']
+        )
+    
+    def _get_model_checkpoint(self):
         return ModelCheckpoint(
-            self.model_route,
+            self.__model_route,
             monitor='val_acc',
             verbose=1,
             save_best_only=True,
@@ -116,24 +104,13 @@ class ImageModel:
             period=1
         )
     
-    @staticmethod
-    def get_early_stop():
-        return EarlyStopping(
-            monitor='val_acc',
-            min_delta=0,
-            patience=10,
-            verbose=1,
-            mode='auto'
-        )
-    
-    @staticmethod
-    def get_directory_iterator(route):
+    def _get_directory_iterator(self, route):
         image_generator = image.ImageDataGenerator(rescale=1.0 / 255)
         
         return image_generator.flow_from_directory(
             directory=route,
-            target_size=(64, 64),
-            batch_size=64,
+            target_size=(self.__width, self.__height),
+            batch_size=self.batch_size,
             class_mode="categorical"
         )
     
