491757df1641063434d1d161d59d1322b217372e
JGuzman <jgfigueira@gradiant.org>
2019-05-13 17:56:16 +0200

    Refactor: changed identation

--- image_feature_extractor/utils.py
+++ image_feature_extractor/utils.py
@@ -3,27 +3,27 @@ import pandas as pd
 
 
 def change_validation_scaffolding(base_route, definition_file, separator):
-  validation_data = _load_validation_data(definition_file, separator)
-  
-  for row in validation_data.iterrows():
-    file = row[1]["file"]
-    label = row[1]["label"]
+    validation_data = _load_validation_data(definition_file, separator)
     
-    label_folder = os.path.join(base_route, label)
-    
-    if not os.path.exists(label_folder):
-      os.mkdir(label_folder)
-    
-    shutil.move(os.path.join(base_route, file), os.path.join(label_folder, file))
+    for row in validation_data.iterrows():
+        file = row[1]["file"]
+        label = row[1]["label"]
+        
+        label_folder = os.path.join(base_route, label)
+        
+        if not os.path.exists(label_folder):
+            os.mkdir(label_folder)
+        
+        shutil.move(os.path.join(base_route, file), os.path.join(label_folder, file))
 
 
 def _load_validation_data(definition_file, separator):
-  validation_data = pd.read_csv(
-    definition_file,
-    sep=separator,
-    header=None
-  )
-  
-  validation_data.columns = ["file", "label", "0", "1", "2", "3"]
-  
-  return validation_data
+    validation_data = pd.read_csv(
+        definition_file,
+        sep=separator,
+        header=None
+    )
+    
+    validation_data.columns = ["file", "label", "0", "1", "2", "3"]
+    
+    return validation_data
