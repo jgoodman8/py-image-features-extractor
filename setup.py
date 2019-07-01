c37832655514cde68f3bbc5fe31ce93d0245d53f
jgoodman8 <jguzmanfd@gmail.com>
2019-07-02 00:57:00 +0200

    Update readme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='py-image-feature-extractor',
-    version='0.0.13',
+    version='0.1.0',
     author='Javier Guzman',
     author_email='jguzmanfd@gmail.com',
     packages=find_packages(),
