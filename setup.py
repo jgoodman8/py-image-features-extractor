d301708c4b2b45e91e8a01ce27e990afe96fa961
jgoodman8 <jguzmanfd@gmail.com>
2020-01-20 00:36:53 +0100

    Add conda env descriptor

--- setup.py
+++ setup.py
@@ -29,18 +29,5 @@ setup(
     long_description=long_description,
     packages=find_packages(),
     entry_points={'console_scripts': ['image_feature_extractor = image_feature_extractor.cli:main']},
-    install_requires=[
-        'click',
-        'numpy',
-        'scikit-image',
-        'tensorflow',
-        'opencv-python==3.4.2.17',
-        'opencv-contrib-python==3.4.2.17',
-        'pandas',
-        'pytest',
-        'matplotlib',
-        'scikit-learn',
-        'scipy',
-    ],
     url='https://github.com/jgoodman8/py-image-feature-selector',
 )
